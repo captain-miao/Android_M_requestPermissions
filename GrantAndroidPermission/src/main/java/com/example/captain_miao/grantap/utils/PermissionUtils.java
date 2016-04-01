4852048ca8971a38b3c0f2dab3fc4558277ef99a
yanlu <caoqinmiao@gmail.com>
2016-04-01 12:35:15 +0800

    improve example

--- GrantAndroidPermission/src/main/java/com/example/captain_miao/grantap/utils/PermissionUtils.java
+++ GrantAndroidPermission/src/main/java/com/example/captain_miao/grantap/utils/PermissionUtils.java
@@ -3,6 +3,7 @@ package com.example.captain_miao.grantap.utils;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings;
@@ -10,9 +11,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.content.PermissionChecker;
 import android.webkit.PermissionRequest;
 
+import com.example.captain_miao.grantap.annotation.PermissionCheck;
 import com.example.captain_miao.grantap.annotation.PermissionDenied;
 import com.example.captain_miao.grantap.annotation.PermissionGranted;
-import com.example.captain_miao.grantap.annotation.PermissionCheck;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -49,17 +50,44 @@ final public class PermissionUtils {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
     }
 
+    /**
+     * Returns true if <code>Activity</code> or <code>Fragment</code> has access to all given permissions.
+     *
+     * @param context     context
+     * @param permissions permissions
+     * @return returns true if <code>Activity</code> or <code>Fragment</code> has access to all given permissions.
+     */
+    @TargetApi(value = Build.VERSION_CODES.M)
+    public static boolean hasSelfPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (isOverMarshmallow() && permission.equals(Manifest.permission.SYSTEM_ALERT_WINDOW)) {
+                if (!Settings.canDrawOverlays(context)) {
+                    return false;
+                }
+            } else if (isOverMarshmallow() && permission.equals(Manifest.permission.WRITE_SETTINGS)) {
+                if (!Settings.System.canWrite(context)) {
+                    return false;
+                }
+            } else if (PermissionChecker.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
     @TargetApi(value = Build.VERSION_CODES.M)
     public static List<String> findDeniedPermissions(Activity activity, String... permission) {
         List<String> denyPermissions = new ArrayList<>();
         for (String value : permission) {
             // CommonsWare's blog post:https://commonsware.com/blog/2015/08/17/random-musings-android-6p0-sdk.html
             //support SYSTEM_ALERT_WINDOW,WRITE_SETTINGS
-            if (value.equals(Manifest.permission.SYSTEM_ALERT_WINDOW)){
+            if (isOverMarshmallow() && value.equals(Manifest.permission.SYSTEM_ALERT_WINDOW)){
                 if(!Settings.canDrawOverlays(activity)){
                     denyPermissions.add(value);
                 }
-            } else if(value.equals(Manifest.permission.WRITE_SETTINGS)){
+            } else if(isOverMarshmallow() && value.equals(Manifest.permission.WRITE_SETTINGS)){
                 if(!Settings.System.canWrite(activity)){
                     denyPermissions.add(value);
                 }
--- app/src/main/java/com/example/captain_miao/permissions/MainActivity.java
+++ app/src/main/java/com/example/captain_miao/permissions/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.captain_miao.permissions;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.captain_miao.grantap.CheckAnnotatePermission;
@@ -16,121 +14,174 @@ import com.example.captain_miao.grantap.annotation.PermissionCheck;
 import com.example.captain_miao.grantap.annotation.PermissionDenied;
 import com.example.captain_miao.grantap.annotation.PermissionGranted;
 import com.example.captain_miao.grantap.listeners.PermissionListener;
+import com.example.captain_miao.grantap.utils.PermissionUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = "MainActivity";
 
 
-    String[] dangerousPermission = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.CAMERA};
 
+    String[] normalPermission = new String[]{Manifest.permission.INTERNET};
 
+    @PermissionCheck()
+    String[] dangerousPermissionLocation = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION};
 
+    String[] dangerousPermissionCamera = new String[]{Manifest.permission.CAMERA};
+    //SYSTEM_ALERT_WINDOW write permission
+    String[] systemAlertWindowPermission = new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW};
+        //system settings write permission
+    String[] settingsPermission = new String[]{Manifest.permission.WRITE_SETTINGS};
+
+    TextView mTvPermissionInternet;
+    TextView mTvPermissionCamera;
+    TextView mTvPermissionLocation;
+    TextView mTvPermissionSystem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
-    //dangerous permission
-    //check permission use CheckPermission
-    public void showRequestPermissionAccessCoarseLocation(View view) {
-        CheckPermission
-                .from(this)
-                .setPermissions(dangerousPermission)
-                .setRationaleConfirmText("Request ACCESS_COARSE_LOCATION")
-                .setDeniedMsg("The ACCESS_COARSE_LOCATION Denied")
-                .setPermissionListener(new PermissionListener() {
-                    @Override
-                    public void permissionGranted() {
-                        Toast.makeText(MainActivity.this, "ACCESS_COARSE_LOCATION Permission Granted", Toast.LENGTH_SHORT).show();
-                    }
+        mTvPermissionInternet = (TextView) findViewById(R.id.tv_permission_internet);
+        mTvPermissionCamera = (TextView) findViewById(R.id.tv_permission_camera);
+        mTvPermissionLocation = (TextView) findViewById(R.id.tv_permission_location);
+        mTvPermissionSystem = (TextView) findViewById(R.id.tv_permission_system);
+        findViewById(R.id.btn_request_permission_all).setOnClickListener(this);
+        findViewById(R.id.btn_request_permission_internet).setOnClickListener(this);
+        findViewById(R.id.btn_request_permission_camera).setOnClickListener(this);
+        findViewById(R.id.btn_request_permission_location).setOnClickListener(this);
+        findViewById(R.id.btn_request_permission_system).setOnClickListener(this);
 
-                    @Override
-                    public void permissionDenied() {
-                        Toast.makeText(MainActivity.this, "ACCESS_COARSE_LOCATION Permission Denied", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .check();
+        updatePermissionStatus();
     }
 
 
 
+    @SuppressWarnings("deprecation")
+    private void updatePermissionStatus() {
+        boolean isGranted = PermissionUtils.hasSelfPermissions(this, normalPermission);
+        mTvPermissionInternet.setText(getString(R.string.normal_permission_status, isGranted ? "Granted" : "Denied"));
+        mTvPermissionInternet.setTextColor(getResources().getColor(isGranted ? R.color.green : R.color.red));
 
-    //normal permission
-    //check permission use CheckAnnotatePermission
-    @PermissionCheck()
-    String[] normalPermission = new String[]{Manifest.permission.INTERNET};
-    public void showPermissionInternet(View view) {
-        CheckAnnotatePermission
-                .from(this, this)
-                .check();
+        isGranted = PermissionUtils.hasSelfPermissions(this, dangerousPermissionCamera);
+        mTvPermissionCamera.setText(getString(R.string.dangerous_permission_status, isGranted ? "Granted" : "Denied"));
+        mTvPermissionCamera.setTextColor(getResources().getColor(isGranted ? R.color.green : R.color.red));
+
+        isGranted = PermissionUtils.hasSelfPermissions(this, dangerousPermissionLocation);
+        mTvPermissionLocation.setText(getString(R.string.dangerous_permission_status, isGranted ? "Granted" : "Denied"));
+        mTvPermissionLocation.setTextColor(getResources().getColor(isGranted ? R.color.green : R.color.red));
+
+        isGranted = PermissionUtils.hasSelfPermissions(this, systemAlertWindowPermission);
+        mTvPermissionSystem.setText(getString(R.string.system_permission_status, isGranted ? "Granted" : "Denied"));
+        mTvPermissionSystem.setTextColor(getResources().getColor(isGranted ? R.color.green : R.color.red));
     }
+
     @PermissionGranted()
     public void permissionGranted() {
-        Toast.makeText(this, "INTERNET Permission Granted", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ACCESS_COARSE_LOCATION Permission Granted", Toast.LENGTH_SHORT).show();
+        updatePermissionStatus();
     }
     @PermissionDenied()
     public void permissionDenied() {
-        Toast.makeText(this, "INTERNET Permission Denied", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ACCESS_COARSE_LOCATION Permission Denied", Toast.LENGTH_SHORT).show();
+        updatePermissionStatus();
     }
 
 
-    //system settings write permission
-    String[] settingsPermission = new String[]{Manifest.permission.WRITE_SETTINGS};
-    private static final int WRITE_SETTINGS_REQUEST_CODE = 8;
-    public void showRequestPermissionWriteSettings(View view) {
-        // for Settings.ACTION_MANAGE_WRITE_SETTINGS: Settings.System.canWrite
-        // CommonsWare's blog post:https://commonsware.com/blog/2015/08/17/random-musings-android-6p0-sdk.html
-        boolean hasSelfPermission = Settings.System.canWrite(this);
-        if(hasSelfPermission) {
-            new AlertDialog.Builder(this)
-                    .setTitle("showPermissionWriteSettings")
-                    .setMessage(settingsPermission[0] + "\r\n" + "granted")
-                    .setPositiveButton(android.R.string.ok, null)
-                    .show();
-        } else {
-            CheckPermission
-                    .from(this)
-                    .setPermissions(dangerousPermission)
-                    .setRationaleConfirmText("Request WRITE_SETTINGS")
-                    .setDeniedMsg("The WRITE_SETTINGS Denied")
-                    .setPermissionListener(new PermissionListener() {
-                        @Override
-                        public void permissionGranted() {
-                            Toast.makeText(MainActivity.this, "WRITE_SETTINGS Permission Granted", Toast.LENGTH_SHORT).show();
-                        }
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_request_permission_all:
+                CheckPermission
+                        .from(this)
+                        .setPermissions(dangerousPermissionCamera[0], dangerousPermissionLocation[0],systemAlertWindowPermission[0], systemAlertWindowPermission[0])
+                        .setRationaleConfirmText("request all permission")
+                        .setDeniedMsg("the permission denied")
+                        .setPermissionListener(new PermissionListener() {
+                            @Override
+                            public void permissionGranted() {
+                                Toast.makeText(MainActivity.this, "all permission granted", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
 
-                        @Override
-                        public void permissionDenied() {
-                            Toast.makeText(MainActivity.this, "WRITE_SETTINGS Permission Denied", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .check();
+                            @Override
+                            public void permissionDenied() {
+                                Toast.makeText(MainActivity.this, "permission denied.", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
+                        })
+                        .check();
+                break;
+            case R.id.btn_request_permission_camera:
+                CheckPermission
+                        .from(this)
+                        .setPermissions(dangerousPermissionCamera)
+                        .setRationaleConfirmText("Request Camera Permission")
+                        .setDeniedMsg("The Camera Permission Denied")
+                        .setPermissionListener(new PermissionListener() {
+                            @Override
+                            public void permissionGranted() {
+                                Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
+
+                            @Override
+                            public void permissionDenied() {
+                                Toast.makeText(MainActivity.this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
+                        })
+                        .check();
+                break;
+            case R.id.btn_request_permission_location:
+                CheckAnnotatePermission
+                        .from(this, this)
+                        .check();
+                break;
+            case R.id.btn_request_permission_system:
+                CheckPermission
+                        .from(this)
+                        .setPermissions(systemAlertWindowPermission)
+                        .setRationaleConfirmText("Request SYSTEM_ALERT_WINDOW")
+                        .setDeniedMsg("The SYSTEM_ALERT_WINDOW Denied")
+                        .setPermissionListener(new PermissionListener() {
+                            @Override
+                            public void permissionGranted() {
+                                Toast.makeText(MainActivity.this, "SYSTEM_ALERT_WINDOW Permission Granted", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
+
+                            @Override
+                            public void permissionDenied() {
+                                Toast.makeText(MainActivity.this, "SYSTEM_ALERT_WINDOW Permission Denied", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
+                        })
+                        .check();
+                break;
+            case R.id.btn_request_permission_internet:
+                CheckPermission
+                        .from(this)
+                        .setPermissions(normalPermission)
+                        .setRationaleConfirmText("Request INTERNET")
+                        .setDeniedMsg("The INTERNET Denied")
+                        .setPermissionListener(new PermissionListener() {
+                            @Override
+                            public void permissionGranted() {
+                                Toast.makeText(MainActivity.this, "INTERNET Permission Granted", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
+
+                            @Override
+                            public void permissionDenied() {
+                                Toast.makeText(MainActivity.this, "INTERNET Permission Denied", Toast.LENGTH_SHORT).show();
+                                updatePermissionStatus();
+                            }
+                        })
+                        .check();
+                break;
         }
     }
-
-    //SYSTEM_ALERT_WINDOW write permission
-    String[] systemAlertWindowPermission = new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW};
-    public void showRequestPermissionAlertWindow(View view) {
-        CheckPermission
-                .from(this)
-                .setPermissions(systemAlertWindowPermission)
-                .setRationaleConfirmText("Request SYSTEM_ALERT_WINDOW")
-                .setDeniedMsg("The SYSTEM_ALERT_WINDOW Denied")
-                .setPermissionListener(new PermissionListener() {
-                    @Override
-                    public void permissionGranted() {
-                        Toast.makeText(MainActivity.this, "SYSTEM_ALERT_WINDOW Permission Granted", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void permissionDenied() {
-                        Toast.makeText(MainActivity.this, "SYSTEM_ALERT_WINDOW Permission Denied", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .check();
-    }
-
 }
